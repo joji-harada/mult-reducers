eef584351e7fb3874329a7f8ec7fcfb5e2e51f97
George Allison <49288165+joji-harada@users.noreply.github.com>
2020-09-02 13:29:56 -0700

    combineReducers implemented. reducerA, reducerB.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 import { connect } from 'react-redux';
 
@@ -9,11 +9,11 @@ class App extends React.Component {
       <div className="App">
         <div className="col">
           <div><span>A:</span><span>{this.props.a}</span></div>
-          <button onClick={this.props.updateA}>Update A</button>
+          <button onClick={() => this.props.updateA(this.props.b)}>Update A</button>
         </div>
         <div className="col">
           <div><span>B:</span><span>{this.props.b}</span></div>
-          <button onClick={this.props.updateB}>Update B</button>
+          <button onClick={() => this.props.updateB(this.props.a)}>Update B</button>
         </div>
       </div>
     )
@@ -22,15 +22,15 @@ class App extends React.Component {
 
 const mapStoreToProps = (store) => {
   return {
-    a: store.a,
-    b: store.b
+    a: store.rA.a,
+    b: store.rB.b
   }
 };
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    updateA: () => dispatch({type: 'UPDATE_A'}),
-    updateB: () => dispatch({type: 'UPDATE_B'})
+    updateA: (b) => dispatch({type: 'UPDATE_A', b:b}),
+    updateB: (a) => dispatch({type: 'UPDATE_B', a:a})
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -4,17 +4,21 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-import reducer from './store/reducer';
-import { createStore } from 'redux';
+import reducerA from './store/reducerA';
+import reducerB from './store/reducerB';
+import { createStore, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
 
-const store = createStore(reducer);
+const rootReducer = combineReducers({
+  rA:reducerA,
+  rB:reducerB
+})
 
-ReactDOM.render(
+const store = createStore(rootReducer);
+
+ReactDOM.render(   
   <Provider store={store}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
+    <App />
   </Provider>,
   document.getElementById('root')
 );
